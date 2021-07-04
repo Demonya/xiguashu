be3072ebabc1e519f966352dc96b5c11b7613643
Demonya <32725399+Demonya@users.noreply.github.com>
2021-07-04 23:44:50 +0800

    Update 2.3性能度量.py

--- Chapter2:模型评估与选择/2.3性能度量.py
+++ Chapter2:模型评估与选择/2.3性能度量.py
@@ -1,33 +1,37 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# ## 性能度量
-# ### 错误率：分类错误的样本数占总体样本数的比例
-# ### 精度：分类正确的样本数占总体样本的比例
-# ### 查准率：真正例占预测样本正例的比例   P = TP/TP+FP
-# ### 查全率：真正例占实际样本正例的比例   R = TP/TP+FN 
-# ### 查准率与查全率是一对矛盾，trade-off。
-# ### PR曲线：查准率为纵轴，查全率为横轴。
-# ### BEP：平衡点（查准率=查全率）
-# ### F1 = 2*P*R/(P+R) 
-# ### Fβ = (1+β^2)*P*R/(β^2*P)+R  考虑β趋于无穷时只有R  考虑β趋于0时只有P 
-# ### 真正例率 = 查全率 = TP/(TP+FN)
-# ### 假正例率 = FP/（FP+TN）
-# ### ROC曲线：真正例率为纵轴，假正例率为横轴。
-# ### AUC：ROC曲线下方面积
+# # coding: utf-8
 # 
+# # ## 性能度量
+# # ### 错误率：分类错误的样本数占总体样本数的比例
+# # ### 精度：分类正确的样本数占总体样本的比例
+# # ### 查准率：真正例占预测样本正例的比例   P = TP/TP+FP
+# # ### 查全率：真正例占实际样本正例的比例   R = TP/TP+FN 
+# # ### 查准率与查全率是一对矛盾，trade-off。
+# # ### PR曲线：查准率为纵轴，查全率为横轴。
+# # ### BEP：平衡点（查准率=查全率）
+# # ### F1 = 2*P*R/(P+R) 
+# # ### Fβ = (1+β^2)*P*R/(β^2*P)+R  考虑β趋于无穷时只有R  考虑β趋于0时只有P 
+# # ### 真正例率 = 查全率 = TP/(TP+FN)
+# # ### 假正例率 = FP/（FP+TN）
+# # ### ROC曲线：真正例率为纵轴，假正例率为横轴。
+# # ### AUC：ROC曲线下方面积
+# # 
 
 # In[1]:
+
 
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,confusion_matrix,f1_score,precision_recall_curve,auc,roc_curve
+from sklearn.metrics import accuracy_score,confusion_matrix,f1_score,precision_recall_curve,roc_curve
 from sklearn.model_selection import train_test_split
 
 
 # In[2]:
+
 
 data,label = load_iris().data[50:150],load_iris().target[50:150]
 label
@@ -35,13 +39,15 @@ label
 
 # In[3]:
 
+
 x_train,x_test,y_train,y_test = train_test_split(data,label,test_size = 0.3,random_state = 0)
 
 
 # In[4]:
 
+
 lr = LogisticRegression()
-lr.fit_transform(x_train,y_train) #sklearn不同版本对应的方法不同fit、fit_transform
+lr.fit(x_train,y_train)
 y_hat = lr.predict(x_test)
 diff = y_hat - y_test
 acc_score = len(diff[diff==0])/len(diff)
@@ -51,6 +57,8 @@ print('sklearn的精度为%.5f'%accuracy_score(y_test,y_hat))
 
 
 # In[5]:
+
+
 
 #计算混淆矩阵
 TP,FN,FP,TN = 0,0,0,0
@@ -75,6 +83,23 @@ print('sklearn 混淆矩阵:' , confusion_matrix(y_test,y_hat).ravel() ,'\n') #s
 
 print('sklearn F1score = ',f1_score(y_test,y_hat,average=None))
 print('查准率= %.5f,查全率= %.5f,F1score= %.5f'%(p,r,F1))
+
+
+# In[6]:
+
+
+lr.predict_proba(x_test)[:,0]
+# y_test
+# y_test.reshape(-1,1)
+
+
+# In[7]:
+
+
+p,r,th = precision_recall_curve(y_test,lr.predict_proba(x_test)[:,0])
+p,r,th
+
+
 
 
 
