d482c558f5b6d687bef00dbc7fb3a5df19623b55
Demonya <32725399+Demonya@users.noreply.github.com>
2021-07-22 14:05:42 +0800

    Update base.py

--- Chapter4:决策树/base.py
+++ Chapter4:决策树/base.py
@@ -1,1 +1,1 @@
-## Markdown <img src="https://latex.codecogs.com/png.image?\dpi{110}&space;h(x)=-log_{2}p(x)" title="h(x)=-log_{2}p(x)" />
+## Markdown <img src="https://latex.codecogs.com/png.image?\dpi{110}&space;\\h(x)=-log_{2}p(x)" title="h(x)=-log_{2}p(x)" />
